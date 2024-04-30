78172a90573ba772a0bd2e6a7e7f932ad4ad3df5
Codster447 <107661192+Codster447@users.noreply.github.com>
2024-04-29 23:13:14 -0500

    Update ISBurnCorpseAction.lua

--- Contents/mods/More Traits/media/lua/client/TimedActions/ISBurnCorpseAction.lua
+++ Contents/mods/More Traits/media/lua/client/TimedActions/ISBurnCorpseAction.lua
@@ -3,7 +3,7 @@ require "TimedActions/ISBaseTimedAction"
 ISBurnCorpseAction = ISBaseTimedAction:derive("ISBurnCorpseAction");
 
 function ISBurnCorpseAction:isValid()
-	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion >= 3 then
+	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion <= 3 then
 		HaloTextHelper.addText(self.character, getText("UI_burnedstop"), HaloTextHelper.getColorRed());
 		return false
 	end
