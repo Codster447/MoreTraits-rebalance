5214a31092df2008bc56e36663cb62bfe31cf0ab
Codster447 <107661192+Codster447@users.noreply.github.com>
2024-04-29 23:13:42 -0500

    Update ISFireplaceLightFromLiterature.lua

--- Contents/mods/More Traits/media/lua/client/TimedActions/ISFireplaceLightFromLiterature.lua
+++ Contents/mods/More Traits/media/lua/client/TimedActions/ISFireplaceLightFromLiterature.lua
@@ -3,7 +3,7 @@ require "TimedActions/ISBaseTimedAction"
 ISFireplaceLightFromLiterature = ISBaseTimedAction:derive("ISFireplaceLightFromLiterature")
 
 function ISFireplaceLightFromLiterature:isValid()
-	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion >= 3 then
+	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion <= 3 then
 		HaloTextHelper.addText(self.character, getText("UI_burnedstop"), HaloTextHelper.getColorRed());
 		return
 	end
