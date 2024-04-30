bba6c1165cadc7569ed72dd258d00afe01612b30
Codster447 <107661192+Codster447@users.noreply.github.com>
2024-04-29 23:23:11 -0500

    Update ISLightFromLiterature.lua

--- Contents/mods/More Traits/media/lua/client/Camping/TimedActions/ISLightFromLiterature.lua
+++ Contents/mods/More Traits/media/lua/client/Camping/TimedActions/ISLightFromLiterature.lua
@@ -4,7 +4,7 @@ require "TimedActions/ISBaseTimedAction"
 ISLightFromLiterature = ISBaseTimedAction:derive("ISLightFromLiterature");
 
 function ISLightFromLiterature:isValid()
-	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion >= 3 then
+	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion <= 3 then
 		HaloTextHelper.addText(self.character, getText("UI_burnedstop"), HaloTextHelper.getColorRed());
 		return
 	end
