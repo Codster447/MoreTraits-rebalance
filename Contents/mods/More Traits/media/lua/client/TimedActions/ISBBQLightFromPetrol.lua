66497aa959fdad5fb3bf77dca1db5511ed37ac4e
Codster447 <107661192+Codster447@users.noreply.github.com>
2024-04-29 23:12:57 -0500

    Update ISBBQLightFromPetrol.lua

--- Contents/mods/More Traits/media/lua/client/TimedActions/ISBBQLightFromPetrol.lua
+++ Contents/mods/More Traits/media/lua/client/TimedActions/ISBBQLightFromPetrol.lua
@@ -3,7 +3,7 @@ require "TimedActions/ISBaseTimedAction"
 ISBBQLightFromPetrol = ISBaseTimedAction:derive("ISBBQLightFromPetrol")
 
 function ISBBQLightFromPetrol:isValid()
-	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion >= 3 then
+	if self.character:HasTrait("burned") and self.character:getModData().MTModVersion <= 3 then
 		HaloTextHelper.addText(self.character, getText("UI_burnedstop"), HaloTextHelper.getColorRed());
 		return
 	end
